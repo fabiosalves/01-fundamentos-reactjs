430464a2a81da8ccc7fcb77255153512f6e13663
Fabio <47723964+fabiosalves@users.noreply.github.com>
2023-04-09 10:29:46 -0300

    [implements form validation]

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,7 +34,12 @@ export function Post({author, publishedAt, content}) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid(){
+    event.target.setCustomValidity('Este campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -48,6 +53,8 @@ export function Post({author, publishedAt, content}) {
     
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return(
     <article className={styles.post}>
@@ -85,9 +92,11 @@ export function Post({author, publishedAt, content}) {
           name="comment"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type='submit'>Comentar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>Comentar</button>
         </footer>
       </form>
 
